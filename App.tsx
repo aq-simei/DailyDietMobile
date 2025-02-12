023b84484faca36989a5b13cd5c3ccbca8f388c3
simei <aq.simei@gmail.com>
2025-02-12 19:40:24 -0300

    feat: setup react query && install dependencies

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { Nunito_400Regular, Nunito_600SemiBold, Nunito_700Bold } from '@expo-google-fonts/nunito';
 import { Routes } from '@navigation/Routes/index';
 import { NavigationContainer } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useFonts } from 'expo-font';
 import { ActivityIndicator } from 'react-native';
 import './global.css';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const queryClient = new QueryClient();
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Nunito_400Regular,
@@ -19,7 +23,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <SafeAreaProvider>
-        <Routes />
+        <QueryClientProvider client={queryClient}>
+          <Routes />
+        </QueryClientProvider>
       </SafeAreaProvider>
     </NavigationContainer>
   );
