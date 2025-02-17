f3af0ab1804ca60c0334b984504b0c20fa911b7e
simei <aq.simei@gmail.com>
2025-02-17 20:10:33 -0300

    feat: expand new meal form to use zod resolver / schema

--- src/Screens/NewMeal/index.tsx
+++ src/Screens/NewMeal/index.tsx
@@ -3,17 +3,33 @@ import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { HomeStackParamList } from '@src/@types/navigation';
 import { SafeScreenContent } from '@src/Components/SafeScreenContent/SafeScreenContent';
 import { Colors } from '@src/Constants/Colors';
-import { ArrowLeft, CircleAlert, CircleCheck } from 'lucide-react-native';
+import { AlertCircle, ArrowLeft, CircleAlert, CircleCheck } from 'lucide-react-native';
 import { useState } from 'react';
-import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import Animated, { SlideInDown, SlideOutDown } from 'react-native-reanimated';
 import { twMerge } from 'tailwind-merge';
 import CustomTextInput from '@src/Components/CustomTextInput/CustomTextInput';
+import { useForm, Controller } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { CreateMealFormSchema, CreateMealFormDTO } from '@src/types/schemas/CreateMealFormSchema';
 
 const NewMeal = () => {
   const { goBack } = useNavigation<NavigationProp<HomeStackParamList>>();
   const [date, setDate] = useState(new Date());
-  const [intoDiet, setIntoDiet] = useState<boolean | null>(null);
+  const [inDiet, setInDiet] = useState<boolean | null>(null);
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<CreateMealFormDTO>({
+    resolver: zodResolver(CreateMealFormSchema),
+  });
+
+  const onSubmit = (data: CreateMealFormDTO) => {
+    console.log(data);
+  };
+
   const onChangeDate = (e: any, selectedDate: any) => {
     const currentDate = selectedDate;
     setDate(currentDate);
@@ -49,47 +65,79 @@ const NewMeal = () => {
           </View>
         </View>
         <View className="shadow-3xl w-full flex-1 rounded-t-2xl bg-base-700 p-6 shadow-base-50">
-          <CustomTextInput
-            onChangeText={() => {}}
-            labelText="Name"
-            placeholder="Enter meal name"
-            autoCapitalize="words"
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange, onBlur, value } }) => (
+              <CustomTextInput
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                labelText="Name"
+                placeholder="Enter meal name"
+                autoCapitalize="words"
+                errorMessage={errors.name?.message}
+              />
+            )}
           />
-          <CustomTextInput
-            onChangeText={() => {}}
-            labelText="Description"
-            placeholder="Enter meal description"
-            textAlignVertical="top"
-            multiline
+          <Controller
+            control={control}
+            name="description"
+            render={({ field: { onChange, onBlur, value } }) => (
+              <CustomTextInput
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                labelText="Description"
+                placeholder="Enter meal description"
+                textAlignVertical="top"
+                multiline
+                errorMessage={errors.description?.message}
+              />
+            )}
           />
           <View className="mb-4 flex flex-row justify-between gap-4">
             <View className="flex-1">
               <Text className="mb-2 font-nunito-bold text-base">Date</Text>
               <TouchableOpacity onPress={showDatepicker}>
-                <TextInput
-                  className="h-12 w-full rounded-md border-2 border-base-300 p-2 font-nunito-semibold text-md"
-                  onPress={showDatepicker}
-                  value={date.toLocaleDateString([], {
-                    day: '2-digit',
-                    month: '2-digit',
-                    year: 'numeric',
-                  })}
-                  editable={false}
-                  focusable
+                <Controller
+                  control={control}
+                  name="date"
+                  render={({ field: { value, onChange } }) => (
+                    <CustomTextInput
+                      labelText="Date"
+                      onChangeText={onChange}
+                      className="h-12 w-full rounded-md border-2 border-base-300 p-2 font-nunito-semibold text-md"
+                      value={date.toLocaleDateString([], {
+                        day: '2-digit',
+                        month: '2-digit',
+                        year: 'numeric',
+                      })}
+                      editable={false}
+                    />
+                  )}
                 />
               </TouchableOpacity>
             </View>
             <View className="flex-1">
               <Text className="mb-2 font-nunito-bold text-base">Time</Text>
               <TouchableOpacity onPress={showTimepicker}>
-                <TextInput
-                  className="h-12 w-full rounded-md border-2 border-base-300 p-2 font-nunito-semibold text-md"
-                  value={date.toLocaleTimeString([], {
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    hour12: true,
-                  })}
-                  editable={false}
+                <Controller
+                  control={control}
+                  name="time"
+                  render={({ field: { value, onChange } }) => (
+                    <CustomTextInput
+                      labelText="Time"
+                      onChangeText={onChange}
+                      className="h-12 w-full rounded-md border-2 border-base-300 p-2 font-nunito-semibold text-md"
+                      value={date.toLocaleTimeString([], {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        hour12: true,
+                      })}
+                      editable={false}
+                    />
+                  )}
                 />
               </TouchableOpacity>
             </View>
@@ -97,29 +145,53 @@ const NewMeal = () => {
           <Text className="mb-2 items-center justify-center font-nunito-bold text-mdi">
             In diet meal?
           </Text>
-          <View className="flex w-full flex-row items-center gap-4">
-            <TouchableOpacity
-              className={twMerge(
-                'flex-1 flex-row items-center justify-center gap-1 rounded-xl py-4',
-                intoDiet === true ? 'border-2 border-green-500 bg-green-100' : 'bg-base-600'
-              )}
-              onPress={() => setIntoDiet(true)}>
-              <CircleCheck color={Colors.green[600]} />
-              <Text className="font-nunito-bold text-mdi">Yes</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              className={twMerge(
-                'flex-1 flex-row items-center justify-center gap-1 rounded-xl py-4',
-                intoDiet === false
-                  ? 'border-2 border-brick-red-600 bg-brick-red-100'
-                  : 'bg-base-600'
-              )}
-              onPress={() => setIntoDiet(false)}>
-              <CircleAlert color={Colors['brick-red'][600]} />
-              <Text className="font-nunito-bold text-mdi">No</Text>
-            </TouchableOpacity>
+          <View className="flex flex-col">
+            <View className="flex w-full flex-row items-center gap-4">
+              <TouchableOpacity
+                className={twMerge(
+                  'flex-1 flex-row items-center justify-center gap-1 rounded-xl py-4',
+                  inDiet === true ? 'border-2 border-green-500 bg-green-100' : 'bg-base-600'
+                )}
+                onPress={() => {
+                  setValue('inDiet', true);
+                  setInDiet(true);
+                }}>
+                <CircleCheck color={Colors.green[600]} />
+                <Text className="font-nunito-bold text-mdi">Yes</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                className={twMerge(
+                  'flex-1 flex-row items-center justify-center gap-1 rounded-xl py-4',
+                  inDiet === false
+                    ? 'border-2 border-brick-red-600 bg-brick-red-100'
+                    : 'bg-base-600'
+                )}
+                onPress={() => {
+                  setValue('inDiet', false);
+                  setInDiet(false);
+                }}>
+                <CircleAlert color={Colors['brick-red'][600]} />
+                <Text className="font-nunito-bold text-mdi">No</Text>
+              </TouchableOpacity>
+            </View>
+            {/* keep selector simple, refactor later for select component */}
+            {errors.inDiet?.message && (
+              <View className="flex flex-row items-center gap-2 mt-1">
+                <AlertCircle
+                  width={16}
+                  height={16}
+                  stroke={Colors['brick-red']['500']}
+                  strokeWidth={2}
+                />
+                <Text className="font-nunito-bold text-brick-red-500">
+                  {errors.inDiet?.message}
+                </Text>
+              </View>
+            )}
           </View>
-          <TouchableOpacity className="mt-auto w-full items-center justify-center rounded-xl bg-base-100">
+          <TouchableOpacity
+            className="mt-auto w-full items-center justify-center rounded-xl bg-base-100"
+            onPress={handleSubmit(onSubmit)}>
             <Text className="p-4 py-6 font-nunito-bold text-mdi text-base-700">
               Record new meal
             </Text>
@@ -129,4 +201,5 @@ const NewMeal = () => {
     </Animated.View>
   );
 };
+
 export { NewMeal };
