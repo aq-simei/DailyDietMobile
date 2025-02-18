11c0786d17eb6532dc4e52a26eff43eeb8148168
simei <aq.simei@gmail.com>
2025-02-17 21:05:13 -0300

    fix: adjust AddMeal mutation

--- src/api/Mutations/AddMeal.ts
+++ src/api/Mutations/AddMeal.ts
@@ -2,8 +2,12 @@ import { axiosInstance } from '../axios';
 import { AddMealRequestDTO, AddMealRoute } from '../../types/dtos/Requests/AddMealRequest';
 
 export const AddMeal = (AddMealRequestDTO: AddMealRequestDTO) => {
-  const response = axiosInstance.post(AddMealRoute, {
-    AddMealRequestDTO,
+  const response = axiosInstance.post(AddMealRoute,  {
+    Description: AddMealRequestDTO.description,
+    Name: AddMealRequestDTO.name,
+    InDiet: AddMealRequestDTO.in_diet,
+    Date: AddMealRequestDTO.date,
+    Time: AddMealRequestDTO.time,
   });
   return response
 };
