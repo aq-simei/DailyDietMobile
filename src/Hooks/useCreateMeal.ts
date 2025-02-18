c2c3ed3fab2dcfa18fea3952cd7e24d70e7a7692
simei <aq.simei@gmail.com>
2025-02-17 21:20:23 -0300

    fix: add navigation on  meal creation success

--- src/Hooks/useCreateMeal.ts
+++ src/Hooks/useCreateMeal.ts
@@ -2,8 +2,11 @@ import { AddMealRequestDTO } from '@src/types/dtos/Requests/AddMealRequest';
 import { AddMeal } from '@src/api/Mutations/AddMeal';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { showInfoToast, showSuccessToast } from '@src/Components/Toasts/Toasts';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { HomeStackParamList } from '@src/@types/navigation';
 
 export const UseCreateMeal = () => {
+  const navigation = useNavigation<NavigationProp<HomeStackParamList>>();
   const queryClient = useQueryClient();
   const {
     mutateAsync: createMeal,
@@ -18,11 +21,12 @@ export const UseCreateMeal = () => {
     onSuccess: () => {
       showSuccessToast('Meal created');
       queryClient.invalidateQueries({ queryKey: ['userMeals'] });
+      navigation.navigate('Home');
     },
     onError: (error) => {
       showInfoToast('Error creating meal');
       console.log(error);
-    }
+    },
   });
   return {
     createMeal,
